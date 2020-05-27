87b9b50b907f1bab7630426e5e777e2ce72e7a36
abt12680 <50388398+abt12680@users.noreply.github.com>
2020-05-27 17:23:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,19 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.111/g' package/base-files/files/bin/config_generate
 
+cd openwrt
+rm -Rf package/lean/luci-theme-argon
+
 #自定插件
+git clone https://github.com/jerrykuku/luci-theme-argon -b 19.07_stable package/luci-theme-argon
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/apollo-ng/luci-theme-darkmatter.git package/luci-theme-darkmatter
+git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
+git clone https://github.com/rosywrt/luci-theme-rosy.git ./package/luci-theme-rosy
+
+#更新feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
